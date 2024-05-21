e84a709751090cbfb5d077b9f0ada8ff3d54901d
Mahdi <mahdimanik994@gmail.com>
2024-05-21 21:49:30 +0600

    layout modifyed firstly

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,37 @@
+import Head from "next/head";
+import Link from "next/link";
+
 const Layout = ({ children }) => {
   return (
-    <div>
-      <header>
-        <nav>
-          <ul>
+    <div className="flex flex-col min-h-screen">
+      <Head>
+        <title>My Blog App</title>
+        <meta
+          name="description"
+          content="A simple blog platform using Next.js"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className="bg-gray-800 p-4">
+        <nav className="flex justify-center">
+          <ul className="flex space-x-4">
             <li>
-              <a href="/">Home</a>
+              <Link href="/">
+                <a className="text-white hover:underline">Home</a>
+              </Link>
             </li>
             <li>
-              <a href="/create">Create Post</a>
+              <Link href="/create">
+                <a className="text-white hover:underline">Create Post</a>
+              </Link>
             </li>
           </ul>
         </nav>
       </header>
-      <main>{children}</main>
+      <main className="flex-1 p-4">{children}</main>
+      <footer className="bg-gray-800 text-white text-center p-4">
+        <p>&copy; {new Date().getFullYear()} My Blog App</p>
+      </footer>
     </div>
   );
 };
